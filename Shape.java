0fcdcbd952fdc9dfa3731fd4c34b962ccc96def7
Abhishek Sharma <100137322+abhishek939089@users.noreply.github.com>
2025-02-05 19:24:24 +0530

    Update Shape.java

--- Shape.java
+++ Shape.java
@@ -5,13 +5,26 @@ public class Shape {
     public double calculatePerimeter(double length, double width) {
         return 2 * (length + width);
     }
+    public double calculateArea(double side) {
+        return side * side;
+    }
+    public double calculatePerimeter(double side) {
+        return 4 * side;
+    }
 
     public static void main(String[] args) {
         Shape shape = new Shape();
+
         double rectangleArea = shape.calculateArea(5, 3);
         double rectanglePerimeter = shape.calculatePerimeter(5, 3);
 
         System.out.println("Rectangle Area: " + rectangleArea);
         System.out.println("Rectangle Perimeter: " + rectanglePerimeter);
+
+        double squareArea = shape.calculateArea(4);
+        double squarePerimeter = shape.calculatePerimeter(4);
+
+        System.out.println("Square Area: " + squareArea);
+        System.out.println("Square Perimeter: " + squarePerimeter);
     }
 }
